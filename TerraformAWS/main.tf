d0f08be6a9cd9521b45463c693f6f7b86aaa3a5e
andervafla <31997112+andervafla@users.noreply.github.com>
2024-10-02 22:13:24 +0300

    update jenks

--- TerraformAWS/main.tf
+++ TerraformAWS/main.tf
@@ -133,7 +133,6 @@ resource "aws_security_group" "backend_sg" {
   }
 }
 
-# Create the frontend EC2 instance with a public IP
 resource "aws_instance" "frontend" {
   ami                    = "ami-0e86e20dae9224db8" 
   instance_type          = "t2.micro"
@@ -147,7 +146,6 @@ resource "aws_instance" "frontend" {
   }
 }
 
-# Створення EC2 інстанса для бекенда
 resource "aws_instance" "backend" {
   ami                    = "ami-0e86e20dae9224db8" 
   instance_type          = "t2.micro"
