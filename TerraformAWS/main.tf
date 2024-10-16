7ae7aab53686dc3184c0654dc2190fbb4a5433ff
andervafla <31997112+andervafla@users.noreply.github.com>
2024-10-16 12:54:31 +0300

    update jenkins

--- TerraformAWS/main.tf
+++ TerraformAWS/main.tf
@@ -125,6 +125,13 @@ resource "aws_security_group" "allow_all_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+   ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
